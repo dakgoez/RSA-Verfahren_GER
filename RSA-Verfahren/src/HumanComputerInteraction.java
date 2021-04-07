c7593cfbbff3465d3e3f41151e73e089340253ba
Diren Akgöz <fibonacci@Steve.local>
2021-04-07 14:31:37 +0200

    Updated HumanComputerInteraction

--- RSA-Verfahren/src/HumanComputerInteraction.java
+++ RSA-Verfahren/src/HumanComputerInteraction.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class HumanComputerInteraction {
+
+    // Konstruktor
+    public HumanComputerInteraction(){}
+
+    Scanner scan = new Scanner(System.in);
+
+    public int enterNumber(){
+        int n = scan.nextInt();
+        return n;
+    }
 }
