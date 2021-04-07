2d40b5d58240d715f5a03526f35fc372d343e492
Diren Akgöz <fibonacci@Steve.local>
2021-04-07 08:40:02 +0200

    Updated Calculations

--- RSA-Verfahren/src/Calculation.java
+++ RSA-Verfahren/src/Calculation.java
@@ -1,4 +1,6 @@
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculation {
 
@@ -10,23 +12,47 @@ public class Calculation {
     *  ob es sich um eine Primzahl handelt oder nicht. */
     public boolean isPrime(int n){
         if (n == 1) return false;
-        for(int i=2;i<n;i++) {
+        for(int i=2; i<n; i++) {
             if(n%i==0) return false;
         }
         return true;
     }
 
-    // mathematische Funktion: größter gemeinsamer Teiler (ggT)
+    // größter gemeinsamer Teiler (ggT)
     /* Die ggT-Methode soll den größten gemeinsamen Teiler zweier Zahlen a und b ermitteln.
     *  Der Algorithmus arbeitet rekursiv. */
     public int ggT(int a, int b){
-        if(a==b||b==0) return a;
+        if(a==b || b==0) return a;
         else return ggT(b,a%b);
     }
 
-    // mathematische Funktion: Phi-Funktion
+    // Primfaktorzerlegung
+    /* Die primFaktor-Methode repräsentiert die Primfaktorzerlegung.
+    *  Alle Primfaktoren einer eingebenen Zahl n werden in einer Liste gespeichert. */
+    public List<Integer> primFaktor(int n){
+        List<Integer> factors = new ArrayList<>();
+        if(isPrime(n)) {factors.add(n); return factors;}
+        for (int i = 1; i <= n/2; i++){
+            if(isPrime(i) && (n%i == 0)) {factors.add(i);}
+        }
+        return factors;
+    }
+
+    public boolean has(List<Integer> l, int a){
+        for (int i : l) {
+            if(i == a) return true;
+        }
+        return false;
+    }
+
+    // Phi-Funktion
     /* Die eulersche Phi-Funktion gibt die Anzahl der teilerfremden Zahlen zur Eingabe n an. */
     public int phi(int n){
-        return 0;
+        if(n == 1) return 1;
+        int count = 0;
+        for(int i = 1; i < n; i++){
+            if(ggT(i,n)==1) count++;
+        }
+        return count;
     }
 }
--- RSA-Verfahren/src/Main.java
+++ RSA-Verfahren/src/Main.java
@@ -1,9 +1,11 @@
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
 
-        Calculation c = new Calculation();
+
 
     }
 }
