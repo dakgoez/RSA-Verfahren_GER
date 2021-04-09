2495a7aee1e19058a98549654041b147a0c201d7
Diren Akgöz <fibonacci@Steve.local>
2021-04-09 03:05:29 +0200

    Updated Calculation

--- RSA-Verfahren/src/Calculation.java
+++ RSA-Verfahren/src/Calculation.java
@@ -44,29 +44,24 @@ public class Calculation {
     // generateRandomKey_E-Methode
     /* Die Methode generateRandomKey_E erstellt anhand einer Eingabe n einen zufällig generierten
     *  öffentlichen Schlüssel e.*/
-    public int generateRandomKey_E(int n){
-        int min = 1;
-        int max = phi(n);
-        int e = 0;
-        while(true){
-            e = (int) (Math.random() * (max - min)) + min;
-            if(ggT(e, phi(n))==1) break;
-        }
-        return e;
+    public int generateRandomKey_E(int n) {
+        int e;
+        do {
+            e = (int) (Math.random() * ((phi(n) - 1) - 2)) + 2;
+            if (ggT(e, phi(n)) == 1) break;
+        } while (true);
+        return  e;
     }
 
     // generateKey_D-Methode
     /* Die Methode generateKey_D erstellt anhand des öffentlichen Schlüssels e und dem Produkt
     *  der eingegebenen Primzahlen p und q einen privaten Schlüssel d. */
     public int generateKey_D(int e, int n){
-        int min = 1;
-        int max = phi(n);
-        int d = 0;
-        while(true){
-            d = (int) (Math.random() * (max - min)) + min;
-            if(d*e%phi(n)==1) break;
+        int x = 0;
+        for (int d = 2; d < phi(n); d++){
+            if(d*e%phi(n)==1) return d;
         }
-        return d;
+        return x;
     }
 
     //Methode: possiblePublicKeys
